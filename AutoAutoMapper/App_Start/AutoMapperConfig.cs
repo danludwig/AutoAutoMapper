68c9c1c3d4f7c677b7c3549519ed544dd09df494
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-09-10 14:34:34 -0400

    Scaffold out functionality.

---
+++ AutoAutoMapper/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using AutoAutoMapper;
+
+namespace MyAppNamespace
+{
+    public static class AutoMapperConfig
+    {
+        public static void RegisterProfiles()
+        {
+            // To register your AutoMapper profiles, add the following line
+            // to the Application_Start method in your Global.asax file:
+            // AutoMapperConfig.RegisterProfiles();
+
+            // By default, AutoAutoMapper will only look for Profiles in the assembly
+            // that calls it (internally it uses Assembly.GetCallingAssembly()).
+            // This means if you call this method via Application_Start in Global.asax,
+            // only your web project will be scanned for AutoMapper.Profile implementations.
+            // If you define AutoMapper.Profile classes in any other assemblies, see
+            // below for instructions on how to tell AutoAutoMapper to scan those assemblies.
+            AutoMapperAutoProfiler.RegisterProfiles();
+
+            // If your AutoMapper Profile classes are defined in a different assembly,
+            // you can register them by passing an assembly argument:
+            //AutoMapperAutoProfiler.RegisterProfiles(Assembly.GetAssembly(typeof(SomeAutoMapperProfileClass)));
+
+            // If you have AutoMapper Profile classes defined in multiple assemblies,
+            // you can pass multiple assembly arguments:
+            // AutoMapperAutoProfiler.RegisterProfiles(Assembly.GetAssembly(typeof(SomeAutoMapperProfileClass)),
+            //    Assembly.GetAssembly(typeof(SomeAutoMapperProfileClassInADifferentAssembly)));
+
+            // Additionally, there is a RegisterProfiles overload that takes an
+            // IEnumerable<Assembly> if you wish to pass all assemblies at once:
+            // var assemblies = new[]
+            //     {
+            //         Assembly.GetAssembly(typeof(SomeAutoMapperProfileClass)),
+            //         Assembly.GetAssembly(typeof(SomeAutoMapperProfileClassInADifferentAssembly)),
+            //     };
+            // AutoMapperAutoProfiler.RegisterProfiles(assemblies);
+        }
+    }
+}
---
+++ AutoAutoMapper/AutoMapperAutoProfiler.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
+namespace AutoAutoMapper
+{
+    /// <summary>
+    /// Helper class for scanning assemblies and automatically adding AutoMapper.Profile
+    /// implementations to the AutoMapper Configuration.
+    /// </summary>
+    public static class AutoMapperAutoProfiler
+    {
+        /// <summary>
+        /// Scans all types in each Assembly argument for AutoMapper Profile classes
+        /// and adds each to the AutoMapper Configuration.
+        /// </summary>
+        /// <param name="assemblies">
+        /// The assemblies to scan for AutoMapper Profile classes. To only scan the
+        /// calling assembly, this argument can be omitted.
+        /// </param>
+        public static void RegisterProfiles(params Assembly[] assemblies)
+        {
+            RegisterProfiles(EnsureAssembly(assemblies, Assembly.GetCallingAssembly()));
+        }
+
+        /// <summary>
+        /// Scans all types in the assemblies argument for AutoMapper Profile classes
+        /// and adds each to the AutoMapper Configuration.
+        /// </summary>
+        /// <param name="assemblies">
+        /// The assemblies to scan for AutoMapper Profile classes. When this argument
+        /// is null or empty, the calling assembly will be scanned by default.
+        /// </param>
+        public static void RegisterProfiles(IEnumerable<Assembly> assemblies)
+        {
+            Mapper.Initialize(configuration =>
+                GetConfiguration(Mapper.Configuration, EnsureAssembly(assemblies, Assembly.GetCallingAssembly())));
+        }
+
+        /// <summary>
+        /// In order for Assembly.GetCallingAssembly() to correctly reference this class' consumer, it must
+        /// be invoked from a public method. Otherwise, Assembly.GetCallingAssembly() will reference this
+        /// assembly, which obviously contains no AutoMapper.Profile implementations.
+        ///
+        /// This is a helper method for substituting the consuming assembly when no other assemblies are
+        /// explicitly passed to any of the public RegisterProfiles method overloads.
+        /// </summary>
+        /// <param name="argAssemblies">
+        /// The assemblies that the consuming assembly explcitly asked us to scan.
+        /// </param>
+        /// <param name="callingAssembly">
+        /// The consuming assembly.
+        /// </param>
+        /// <returns>
+        /// An enumeration of assemblies to be scanned for AutoMapper.Profile class implementations.
+        /// </returns>
+        private static IEnumerable<Assembly> EnsureAssembly(IEnumerable<Assembly> argAssemblies, Assembly callingAssembly)
+        {
+            // In order for GetCallingAssembly to work, it must be invoked from a public method.
+            // This is why the callingAssembly arg is passed from public methods.
+
+            // create an initial default empty array of assemblies in case argAssemblies is null
+            var assembliesArray = new Assembly[] { };
+
+            // when argAssemblies is not null, enumerate it as an array to prevent multiple enumerations
+            if (argAssemblies != null)
+                assembliesArray = argAssemblies.ToArray();
+
+            // when there are no assemblies explicitly defined, return the calling assembly only
+            if (!assembliesArray.Any())
+                return new[] { callingAssembly };
+
+            // otherwise, return the explicitly defined assemblies
+            return assembliesArray;
+        }
+
+        /// <summary>
+        /// Enumerates over all relevant assemblies, scanning each for non-abstract implementations of
+        /// AutoMapper.Profile. For each match that is found, a new instance is constructed and added
+        /// to the AutoMapper configuration.
+        /// </summary>
+        /// <param name="configuration">
+        /// The AutoMapper configuration.
+        /// </param>
+        /// <param name="assemblies">
+        /// Assemblies to scan for concrete AutoMapper.Profile implementations.
+        /// </param>
+        private static void GetConfiguration(IConfiguration configuration, IEnumerable<Assembly> assemblies)
+        {
+            foreach (var assembly in assemblies)
+            {
+                var profileClasses = assembly.GetTypes()
+                    .Where(t =>
+                        t != typeof(Profile)
+                        && typeof(Profile).IsAssignableFrom(t)
+                        && !t.IsAbstract
+                    )
+                    .ToArray()
+                ;
+                foreach (var profileClass in profileClasses)
+                {
+                    configuration.AddProfile((Profile)Activator.CreateInstance(profileClass));
+                }
+            }
+        }
+    }
+}
